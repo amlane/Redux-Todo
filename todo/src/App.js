ad75047c267f2930d46c79a4241c0f31c9b6c16f
amlane <twelvexstring@gmail.com>
2019-05-13 18:15:30 -0500

    added an animation cuz why not?

--- todo/src/App.js
+++ todo/src/App.js
@@ -5,8 +5,8 @@ import ToDoList from './components/ToDoList';
 
 function App() {
   return (
-    <div className="App">
-      <h1>To Do List</h1>
+    <div className="App glowing">
+      <h1>To Do App</h1>
       <ToDoList />
     </div>
   );
--- todo/src/components/ToDoList.js
+++ todo/src/components/ToDoList.js
@@ -19,12 +19,12 @@ class ToDoList extends React.Component {
 
     render(){
         return (
-            <div>
+            <div className="form-container">
                 <ul>{this.props.todos.map(todo => (
                     <ToDo key={todo} todo={todo} />
                 ))}</ul>
                 <form onSubmit={this.addToDo}>
-                <input onChange={this.handleInputChanges} value={this.state.newToDo} placeholder="add a to do" />
+                <input onChange={this.handleInputChanges} value={this.state.newToDo} placeholder="add new to do..." />
                 <button>Add</button>
                 </form>
             </div>
