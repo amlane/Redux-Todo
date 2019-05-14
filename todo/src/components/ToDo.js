cf86818d4b2203fba46c3c7fc6ea7641afdf0c0d
amlane <twelvexstring@gmail.com>
2019-05-14 15:16:05 -0500

    got toggle working in the console!

--- todo/src/components/ToDo.js
+++ todo/src/components/ToDo.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
 export default props => {
-    console.log(props.todo)
+    console.log(props.todo.completed)
     return (
         <>
-        <li>{props.todo.value} <button className="dlt-btn">x</button></li>
+        <li key={props.todo.id} onClick={() => props.toggleComplete(props.todo.id)}>{props.todo.value} <button className="dlt-btn">x</button></li>
         </>
     )
 
